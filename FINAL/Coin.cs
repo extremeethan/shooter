89519da36daa8173c1dae272eccf052ca1db7aac
Ethan <89879031+extremeethan@users.noreply.github.com>
2025-11-17 17:58:16 -0500

    Create Coin.cs

---
+++ FINAL/Coin.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public GameObject coinPrefab;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        Destroy(gameObject, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if (whatDidIHit.tag == "Player")
+        {
+            gameManager.AddScore(5);
+            gameManager.PlaySound(3);
+            Destroy(this.gameObject);
+        }
+    }
+}
