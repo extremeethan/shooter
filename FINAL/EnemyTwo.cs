520153883d30a641f872f27d82c52a7d601dc556
Ethan <89879031+extremeethan@users.noreply.github.com>
2025-11-17 18:01:18 -0500

    Update EnemyTwo.cs

--- FINAL/EnemyTwo.cs
+++ FINAL/EnemyTwo.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyTwo : MonoBehaviour
 {
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
